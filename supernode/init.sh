f31c7eba290ccdddc7f0d4bddd6805f0c19d1209
blockgod666 <1689674986@qq.com>
2019-08-12 20:58:28 +0800

    ...

--- supernode/init.sh
+++ supernode/init.sh
@@ -3,9 +3,11 @@ wget https://raw.githubusercontent.com/blockgod666/vcc/master/supernode/start.sh
 wget https://raw.githubusercontent.com/blockgod666/vcc/master/supernode/download.sh
 wget https://raw.githubusercontent.com/blockgod666/vcc/master/supernode/restart.sh
 wget https://raw.githubusercontent.com/blockgod666/vcc/master/supernode/stop.sh
+wget https://raw.githubusercontent.com/blockgod666/vcc/master/supernode/onekey.sh
 
 chmod +x attach.sh
 chmod +x start.sh
 chmod +x download.sh
 chmod +x restart.sh
 chmod +x stop.sh
+chmod +x onekey.sh
