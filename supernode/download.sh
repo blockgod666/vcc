535f853d194c051d42f1a11942760f3011fb07d8
blockgod666 <54021477+blockgod666@users.noreply.github.com>
2019-09-17 17:13:20 +0800

    upgrade to v0.2.10

--- supernode/download.sh
+++ supernode/download.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/cryptozoic-io/support/master/downloads/gvc/gvc-stable-0.2.9-linux.tar
+wget https://raw.githubusercontent.com/cryptozoic-io/support/master/downloads/gvc/gvc-stable-0.2.10-linux.tar
 rm -rf gvc
-tar -vxf gvc-stable-0.2.9-linux.tar
+tar -vxf gvc-stable-0.2.10-linux.tar
 echo "unzip successfully"
